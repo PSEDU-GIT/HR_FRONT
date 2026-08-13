c3852828a56a45ba68679912a538a72f7df61964
byoungyoon <bur5698@naver.com>
2026-08-13 17:33:03 +0900

    :fix: 희망금액 수정 문구 수정

--- src/app/(afterLogin)/wizard/(standard)/step2/_area/level3/Step2SalaryMonthly.area.tsx
+++ src/app/(afterLogin)/wizard/(standard)/step2/_area/level3/Step2SalaryMonthly.area.tsx
@@ -5,10 +5,10 @@ export default function Step2SalaryMonthlyArea() {
     <div className="space-y-3 pt-2">
       <div>
         <legend className="text-text-side text-xs font-extrabold tracking-widest uppercase dark:text-slate-300">
-          강사에게 매월 지급하고자 하는 월 지급액은 얼마인가요?
+          강사에게 매월 지급하고자 하는 총 지급 희망금액은 얼마인가요?
         </legend>
         <p className="text-text-sub mt-1 text-[11px] font-semibold dark:text-slate-400">
-          * 추가로 붙는 수당 제외
+          * 주휴수당, 식대, 추가 수당 등이 모두 포함된 최종 지급 희망 금액입니다.
         </p>
       </div>
       <FormMonthlySalaryAction />
